5ecb972ab3f6223d606376cf6b913bc11cdf55e1
priscillayvf <priscillayf@gmail.com>
2019-10-04 13:12:46 -0600

    Add filters.

--- frontend-fm/app.js
+++ frontend-fm/app.js
@@ -1,17 +1,30 @@
 const body = document.body
 // const selectTag = document.querySelector('#selectTag')
-const cardContainer = document.createElement('div')
+// const flipCard = document.querySelector("#flipCard")
+const form = document.querySelector(".forms")
 const sFilter = document.querySelector("#species")
 const aFilter = document.querySelector("#age")
 const gFilter = document.querySelector("#gender")
 const submit = document.querySelector("#submit")
+const header = document.querySelector(".header")
+
+
+
+
+
 let thePets = []
 let sResult = ""
 let aResult = ""
 let gResult = ""
 
+function allPets(pets){
+  thePets = pets
+}
 
-cardContainer.className = 'cardContainer'
+fetch('http://localhost:3000/pets')
+.then(response => response.json())
+.then(pets => createCard(pets))
+
 
 sFilter.addEventListener("change", (filter) => {
   sResult = filter.target.value
@@ -27,132 +40,123 @@ gFilter.addEventListener("change", (filter) => {
 
 submit.addEventListener("click", event => {
   event.preventDefault()
-  petFilter(thePets)
+  fetch('http://localhost:3000/pets')
+  .then(response => response.json())
+  .then(result => petFilter(result))
 })
 
 
 
 
-// function createCard(pets) {
-//     // let flipCard = document.querySelector('#flipCard')
-//     pets.forEach(pet => {
-    
+function createCard(pets) {
+  const cardContainer = document.createElement('div')
+  cardContainer.className = 'cardContainer'
 
-//         let flipCard = document.createElement('a')
-//         let flipCardBack = document.createElement('div')
-//         let flipCardFront = document.createElement('div')
-//         let flipCardInner = document.createElement('div')
-        
-//         flipCard.href = `cats.html`
-
-//         flipCardFront.className = 'flipCardFront'
-//         flipCardBack.className = "flipCardBack"
-//         flipCardInner.className = 'flipCardInner'
-        
-//         let name = document.createElement('h3')
-//         let breed = document.createElement('p')
-//         let gender = document.createElement('p')
-//         let age = document.createElement('p')
-//         let image = document.createElement('img')
-        
-//         name.innerText = pet.name
-//         breed.innerText = pet.breed
-//         gender.innerText = pet.gender
-//         age.innerText =  ' Age: ' + pet.age
-//         image.src = pet.image
-
-// // console.log(pet)
+    pets.forEach(pet => {
     
-//     flipCardFront.append(image)
-//     flipCardBack.append(name, breed, age, gender)
-//     flipCardInner.append(flipCardBack, flipCardFront)    
-//     flipCard.append(flipCardInner)
-//     cardContainer.append(flipCard)
-//     body.append(cardContainer)
+        // let flipCardBack = document.createElement('div')
+        // let flipCardFront = document.createElement('div')
+        // let flipCardInner = document.createElement('div')
+        
+        // //flipCard.href = `cats.html`
+
+        // flipCardFront.className = 'flipCardFront'
+        // flipCardBack.className = "flipCardBack"
+        // flipCardInner.className = 'flipCardInner'
+        let name = document.createElement('h3')
+        let breed = document.createElement('p')
+        let gender = document.createElement('p')
+        let age = document.createElement('p')
+        let image = document.createElement('img')
+        let flipCard = document.createElement("div")
+        let flipCardBack = document.createElement('div')
+        let flipCardFront = document.createElement('div')
+        let flipCardInner = document.createElement('div')
+
+        flipCardFront.className = 'flipCardFront'
+        flipCardBack.className = "flipCardBack"
+        flipCardInner.className = 'flipCardInner'
+        flipCard.classList.add("flipCard")
+        name.innerText = pet.name
+        breed.innerText = pet.breed
+        gender.innerText = pet.gender
+        age.innerText =  ' Age: ' + pet.age
+        image.src = pet.image
+
+      
+        flipCardFront.appendChild(image)
+        flipCardBack.append(name, breed, age, gender)
+        flipCardInner.append(flipCardBack, flipCardFront)    
+        flipCard.appendChild(flipCardInner)
+        cardContainer.append(flipCard)
+        body.append(cardContainer)
+
+      
     
-// })
- 
-// }
+  })
+}
 
 
 function petFilter(pets){
-  pets.filter(pet => pet.species == sResult).forEach(pet => {
+  body.innerHTML = ""
 
-    body.innerHTML = ""
 
-    let flipCard = document.createElement('a')
+
+  let genAge = ""
+  const cardContainer = document.createElement('div')
+  cardContainer.className = 'cardContainer'
+
+  pets.filter(pet => pet.species == sResult).filter(pet => pet.gender.includes(gResult)).forEach(pet => {
+    
+    // flipCard.href = `cats.html`
+    let flipCard = document.createElement("div")
     let flipCardBack = document.createElement('div')
     let flipCardFront = document.createElement('div')
     let flipCardInner = document.createElement('div')
+  
+    flipCard.innerHTML = ""
+    flipCardBack.innerHTML = ""
+    flipCardFront.innerHTML = ""
+    flipCardInner.innerHTML = ""
     
-    // flipCard.href = `cats.html`
-
-    flipCardFront.className = 'flipCardFront'
-    flipCardBack.className = "flipCardBack"
-    flipCardInner.className = 'flipCardInner'
-    
-
     let name = document.createElement('h3')
     let breed = document.createElement('p')
     let gender = document.createElement('p')
     let age = document.createElement('p')
     let image = document.createElement('img')
-      
 
-    if (pet.gender.includes(gResult)) {
-      if(aResult == "young" && pet.age < 4) {
-        name.innerText = pet.name
-        breed.innerText = pet.breed
-        gender.innerText = pet.gender
-        age.innerText =  ' Age: ' + pet.age
-        image.src = pet.image
-        flipCardBack.append(name, breed, age, gender)
-      }
-      else if (aResult == "adult" &&  4 < pet.age < 7) {
-        name.innerText = pet.name
-        breed.innerText = pet.breed
-        gender.innerText = pet.gender
-        age.innerText =  ' Age: ' + pet.age
-        image.src = pet.image
-        flipCardBack.append(name, breed, age, gender)
-      }
-      else {
-        name.innerText = pet.name
-        breed.innerText = pet.breed
-        gender.innerText = pet.gender
-        age.innerText =  ' Age: ' + pet.age
-        image.src = pet.image
-        flipCardBack.append(name, breed, age, gender)
-      }
-
+    flipCardFront.className = 'flipCardFront'
+    flipCardBack.className = "flipCardBack"
+    flipCardInner.className = 'flipCardInner'
+    flipCard.classList.add("flipCard")
+    
+    if (pet.age < 4) {
+      genAge = "young"
+    }
+    else if (4 < pet.age && pet.age < 8) {
+      genAge = "adult"
+    }
+    else {
+      genAge = "senior"
     }
 
+    if (aResult == genAge) {
+      name.innerText = pet.name
+      breed.innerText = pet.breed
+      gender.innerText = pet.gender
+      age.innerText =  ' Age: ' + pet.age
+      image.src = pet.image
+      flipCardBack.append(name, breed, age, gender)
+      flipCardFront.appendChild(image)
+      flipCardInner.append(flipCardBack, flipCardFront)    
+      flipCard.append(flipCardInner)
+      cardContainer.append(flipCard)
+    }
 
-    flipCardFront.append(image)
-    flipCardInner.append(flipCardBack, flipCardFront)    
-    flipCard.append(flipCardInner)
-    cardContainer.append(flipCard)
-
-  
   })
-  body.append(cardContainer)
+  body.append(header, form, cardContainer)
+  
 }
-
-
-
-function allPets(pets){
-  thePets = pets
-}
-
-
-// fetch('http://localhost:3000/pets')
-// .then(response => response.json())
-// .then(pets => createCard(pets))
-   
-
-fetch('http://localhost:3000/pets')
-.then(response => response.json())
-.then(result => allPets(result))
    
 
 
